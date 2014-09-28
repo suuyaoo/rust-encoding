fe99249202936fd6d9b8e5e460c569627f68f807
suuyaoo <suuyaoo@gmail.com>
2014-09-29 01:18:11 +0800

    nightly

--- src/encoding/types.rs
+++ src/encoding/types.rs
@@ -125,7 +125,7 @@ impl StringWriter for String {
     }
 
     fn write_char(&mut self, c: char) {
-        self.push_char(c);
+        self.push(c);
     }
 
     fn write_str(&mut self, s: &str) {
